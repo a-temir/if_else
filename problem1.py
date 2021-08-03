09fc88fe6a6c2c53424e57bba4ded0c3731ec1a3
a-temir <asely9@gmail.com>
2021-08-03 19:18:18 +0600

    if_else

---
+++ problem1.py
@@ -0,0 +1,6 @@
+a = 2**3
+b = 3**2
+if a < b:
+	print('a больше b')
+elif a > b:
+	print('a меньше b')
---
+++ problem4.py
@@ -0,0 +1,8 @@
+a = 5
+b = 15
+if a>=b:
+	print(a,'больше',b)
+elif a<=b:
+	print(a,'меньше',b)
+else:
+	print:(a,'равно',b)
